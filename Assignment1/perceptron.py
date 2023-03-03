95a9ca3360258f2a04979dc26b40161b65819842
SnowCharm <619022098@qq.com>
2023-03-03 20:05:57 +0800

    shuffle the data and test

--- Assignment1/perceptron.py
+++ Assignment1/perceptron.py
@@ -39,6 +39,15 @@ class Perceptron(object):
                 self.weights[0] += self.lr * np.sum(y[mask])
                 self.weights[1:] += self.lr * np.dot(y[mask], x[mask])
 
+    def test(self, testing_inputs, labels):
+        cnt = 0
+        for x, y in zip(testing_inputs, labels):
+            pred = self.forward(x)
+            pred = np.where(pred >= 0, 1, -1)
+            if int(pred) == int(y):
+                cnt += 1
+        return cnt
+
 
 mean1 = [0, 0]
 cov1 = [[1, 0], [0, 1]]
@@ -53,5 +62,16 @@ train_labels = np.concatenate((np.ones(80), -np.ones(80)))
 test_data = np.concatenate((data1[80:], data2[80:]))
 test_labels = np.concatenate((np.ones(20), -np.ones(20)))
 
+shuffle_index = np.random.permutation(len(train_data))
+train_data = train_data[shuffle_index]
+train_labels = train_labels[shuffle_index]
+
+shuffle_index = np.random.permutation(len(test_data))
+test_data = test_data[shuffle_index]
+test_labels = test_labels[shuffle_index]
+
 model = Perceptron(2)
 model.train(train_data, train_labels)
+cnt = model.test(test_data, test_labels)
+print("The number of data points is {}, the number of correct predictions is {}, "
+      "the classification accuracy is {}".format(40, cnt, cnt / 40))
