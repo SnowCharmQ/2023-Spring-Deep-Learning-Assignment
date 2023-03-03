5adfcbcffcdf93e51de622cecc576342eb46144a
SnowCharm <619022098@qq.com>
2023-03-03 19:30:14 +0800

    implement generating the data

--- Assignment1/perceptron.py
+++ Assignment1/perceptron.py
@@ -18,11 +18,11 @@ class Perceptron(object):
 
     def forward(self, input):
         """
-        Predict label from input 
+        Predict label from input
         Args:
             input: array of dimension equal to n_inputs.
         """
-        label = np.matmul(self.weights[1:], input) + self.weights[0]
+        label = np.dot(self.weights[1:], input) + self.weights[0]
         return label
 
     def train(self, training_inputs, labels):
@@ -37,4 +37,21 @@ class Perceptron(object):
                 pred = self.forward(x)
                 mask = y * pred <= 0
                 self.weights[0] += self.lr * np.sum(y[mask])
-                self.weights[1:] += self.lr * np.outer(y[mask], x[mask])
+                self.weights[1:] += self.lr * np.dot(y[mask], x[mask])
+
+
+mean1 = [0, 0]
+cov1 = [[1, 0], [0, 1]]
+mean2 = [3, 3]
+cov2 = [[1, 0], [0, 1]]
+
+data1 = np.random.multivariate_normal(mean1, cov1, 100)
+data2 = np.random.multivariate_normal(mean2, cov2, 100)
+
+train_data = np.concatenate((data1[:80], data2[:80]))
+train_labels = np.concatenate((np.ones(80), -np.ones(80)))
+test_data = np.concatenate((data1[80:], data2[80:]))
+test_labels = np.concatenate((np.ones(20), -np.ones(20)))
+
+model = Perceptron(2)
+model.train(train_data, train_labels)
