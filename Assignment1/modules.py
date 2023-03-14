fix a bug in SoftMax backward

Assignment1/modules.py
@@ -49,7 +49,7 @@ class SoftMax(object):
         dx = -self.out[:, :, None] * self.out[:, None, :]
         dx[:, np.arange(self.out.shape[1]), np.arange(self.out.shape[1])] = self.out * (1 - self.out)
         d_result = np.matmul(dout[:, None, :], dx)
-        d_result = np.squeeze(d_result, axis=0)
+        d_result.resize((d_result.shape[-3], d_result.shape[-1]))
         return d_result
 
 