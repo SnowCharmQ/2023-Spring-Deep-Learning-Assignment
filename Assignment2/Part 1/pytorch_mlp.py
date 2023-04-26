9120d6d3ea89f5bffb8abdf2bfba55e0aaeedc8e
SnowCharm <619022098@qq.com>
2023-04-27 01:05:36 +0800

    update part1 pytorch MLP implementation

--- Assignment2/Part 1/pytorch_mlp.py
+++ Assignment2/Part 1/pytorch_mlp.py
@@ -5,12 +5,6 @@ from __future__ import print_function
 import torch.nn as nn
 
 
-def init_weights(m):
-    if type(m) == nn.Linear:
-        nn.init.normal_(m.weight, mean=0, std=0.0001)
-        m.bias.data.fill_(0.0)
-
-
 class MLP(nn.Module):
 
     def __init__(self, n_inputs, n_hidden, n_classes):
@@ -22,8 +16,6 @@ class MLP(nn.Module):
             self.layers.add_module('relu' + str(i), nn.ReLU())
             in_features = out_features
         self.layers.add_module('linear' + str(len(n_hidden)), nn.Linear(in_features, n_classes))
-        self.layers.add_module('softmax', nn.Softmax(dim=1))
-        self.layers.apply(init_weights)
 
     def forward(self, x):
         out = self.layers(x)
